f36ed92bc969fa1f4b9b0af34b9a8055c00b1cdd
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-01 00:10:21 +0300

    Fixed
    Reduced the number of teams: (cout << " ") end (cout << "*")

--- For checking/Classes №1/1.7/main.cpp
+++ For checking/Classes №1/1.7/main.cpp
@@ -4,29 +4,24 @@ using namespace std;
 
 int main()
 {
-    cout<<"Enter the size of the Christmas tree>>";
-    int sizze;
-    cin>>sizze;
-    int k=sizze-1;
+cout<<"Enter the size of the Christmas tree>>";
+int sizze;
+cin>>sizze;
+int k=sizze-1;
     for (int i=0;i<sizze;i++)
     {
-    for (int j=k;j>i;j--)
-    {
-        cout<<" ";
+        for (int j=k;j>i;j--)
+        {
+            cout<<" ";
+        }
+        for (double q=-0.5;q<i;)
+        {
+            cout<<"Y";
+            q=q+0.5;
+        }
+        cout<<endl;
     }
-    cout<<"*";
-    for (int q=0;q<i;q++)
-    {
-        cout<<"*";
-
-    }
-    for (int j=0;j<i;j++)
-    {
-        cout<<"*";
-    }
-    cout<<endl;
-    }
-for (int i=1;i<sizze;i++)
+    for (int i=1;i<sizze;i++)
     {
         cout<<" ";
     }
