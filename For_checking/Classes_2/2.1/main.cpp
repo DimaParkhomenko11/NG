1c46888b0e7a6d7e9da6d0a7a2ed00ebd469bef7
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-05 01:12:20 +0300

    Remotely command break

--- For_checking/Classes_2/2.1/main.cpp
+++ For_checking/Classes_2/2.1/main.cpp
@@ -6,6 +6,7 @@ int main()
 {
  int school[10];
  int mySchool;
+ int i=0;
     for (int i=0;i<10;i++)
     {
         cout<<"Vvedi nomer kakoi-to shkolu:";
@@ -13,18 +14,17 @@ int main()
     }
     cout<<"A teper vvedi svoyu shkolu:";
     cin>>mySchool;
-    for (int i=0;i<10;i++)
-    {
-        if (mySchool==school[i])
+
+
+        if ((mySchool==school[0])||(mySchool==school[1])||(mySchool==school[2])||(mySchool==school[3])||(mySchool==school[4])||(mySchool==school[5])||(mySchool==school[6])||(mySchool==school[7])||(mySchool==school[8])||(mySchool==school[9]))
         {
             cout<<"Ya znayu etu shkolu!";
-            break;
         }
         else
         {
             cout<<"Ya ne znayu etu shkolu!";
-            break;
+
         }
-    }
+
     return 0;
 }
