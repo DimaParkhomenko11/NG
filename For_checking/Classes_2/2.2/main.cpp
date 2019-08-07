1ad0dc155aebd913900c5e611c0a919f8af0960c
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-08 00:30:40 +0300

    Added validation on input data

--- For_checking/Classes_2/2.2/main.cpp
+++ For_checking/Classes_2/2.2/main.cpp
@@ -7,18 +7,23 @@ int main()
     int card[10] = {0,0,0,0,0,0,0,0,0,0};
     int N;
     int sum;
-
     for (;;) {
-
     cout<<"Vvedi nomer kartochki: ";
     cin>>N;
-    cout<<"Skolko polozhit: ";
-    cin>>sum;
-    card[N-1]+=sum;
+    if ((N==1)||(N==2)||(N==3)||(N==4)||(N==5)||(N==6)||(N==7)||(N==8) ||(N==9)||(N==10))
+    {
 
-    for (int i=0;i<10;i++)
-        cout<<card[i]<<" ";
-    cout<<endl;
+        cout<<"Skolko polozhit: ";
+        cin>>sum;
+        card[N-1]+=sum;
+        for (int i=0;i<10;i++)
+            cout<<card[i]<<" ";
+        cout<<endl;
+    }
+    else
+    {
+        cout<<"Error: you entered incorrect values!!!\n";
+    }
 }
     return 0;
 }
