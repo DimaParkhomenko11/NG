d6f316662e870f84c53e36fa140bfbb313dde94c
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-09 00:44:48 +0300

    Added: limited input of numbers, checking for paired numbers

--- For_checking/Classes_2/2.3/main.cpp
+++ For_checking/Classes_2/2.3/main.cpp
@@ -7,19 +7,32 @@ int main()
     int number[20]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int i=-1;
     int sizze=0;
+    int End=0;
     do
     {
         i++;
         cin>>number[i];
-
+        if (number[i]>0)
+        {
+            if ((number[i]%2==0))
+                {
+                    cout<<"Error: you entered a pair number!!!";
+                    End=1;
+                    number[i]=0;
+                }
+        }
+        if (i==19)
+            number[i]=0;
     }while(number[i]!=0);
-
+    cout<<endl;
+    if (End==0)
+    {
     for (int i=0;i<20;i++)
     {
        if (number[i]>0)
            sizze++;
     }
-   int numberMax=number[0];
+    int numberMax=number[0];
     for (int i=1; i<20; i++)
     {
         if (number[i]>numberMax)
@@ -37,6 +50,8 @@ int main()
             cout<<"*";
         }
         cout<<endl;
+    }
+
     }
     return 0;
 }
