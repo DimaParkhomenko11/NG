cec007022a0eee66867f08d350239b0b2696677f
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-11 21:15:58 +0300

    A copy of the input array is created and changes pass with it

--- For_checking/Classes_2/2.4/main.cpp
+++ For_checking/Classes_2/2.4/main.cpp
@@ -6,26 +6,29 @@ int main()
 {
     int i;
     int stolbik[5];
+    int prototip[5];
     for(i=0;i<5;i++)
     {
         cout<<"Zvezdochek v "<<i+1<<"-om stolbike:";
         cin>>stolbik[i];
+        prototip[i]=stolbik[i];
     }
-    int Max=stolbik[0];
+
+    int Max=prototip[0];
     for (i=1; i<5; i++)
     {
-        if (stolbik[i]>Max)
-            Max=stolbik[i];
+        if (prototip[i]>Max)
+            Max=prototip[i];
     }
     for (int q=0;q<Max;q++)
     {
     for (i=0;i<5;i++)
     {
-        if (stolbik[i]>0)
+        if (prototip[i]>0)
             cout<<"*";
         else
             cout<<" ";
-    stolbik[i]+=-1;
+   prototip[i]+=-1;
     }
     cout<<endl;
     }
