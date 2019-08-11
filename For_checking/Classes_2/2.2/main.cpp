dccc93c9b85d749d3a4cc7fc30c18bbd305b91db
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-11 21:08:58 +0300

    The number of credit cards increased to 100

--- For_checking/Classes_2/2.2/main.cpp
+++ For_checking/Classes_2/2.2/main.cpp
@@ -4,26 +4,27 @@ using namespace std;
 
 int main()
 {
-    int card[10] = {0,0,0,0,0,0,0,0,0,0};
-    int N;
+    int card[100];
+    int N,i,k=0;
     int sum;
+    for (i=0;i<100;i++)
+    card[i]=0;
     for (;;) {
     cout<<"Vvedi nomer kartochki: ";
     cin>>N;
-    if ((N==1)||(N==2)||(N==3)||(N==4)||(N==5)||(N==6)||(N==7)||(N==8) ||(N==9)||(N==10))
-    {
-
-        cout<<"Skolko polozhit: ";
-        cin>>sum;
-        card[N-1]+=sum;
-        for (int i=0;i<10;i++)
+    if ((N>0)&&(N<101))
+        {
+            cout<<"Skolko polozhit: ";
+            cin>>sum;
+            card[N-1]+=sum;
+            for (int i=0;i<100;i++)
             cout<<card[i]<<" ";
-        cout<<endl;
-    }
-    else
-    {
-        cout<<"Error: you entered incorrect values!!!\n";
-    }
+            cout<<endl;
+        }
+        else
+        {
+            cout<<"Error: you entered incorrect values!!!\n";
+        }
 }
     return 0;
 }
