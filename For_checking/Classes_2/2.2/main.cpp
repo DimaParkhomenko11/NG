5ad00ff0b01c11f8e7fe920e5adecfe44488b767
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-11 21:26:02 +0300

    Added Code Indentation

--- For_checking/Classes_2/2.2/main.cpp
+++ For_checking/Classes_2/2.2/main.cpp
@@ -4,27 +4,30 @@ using namespace std;
 
 int main()
 {
-    int card[100];
-    int N,i,k=0;
-    int sum;
+ int card[100];
+ int N,i,k=0;
+ int sum;
     for (i=0;i<100;i++)
-    card[i]=0;
-    for (;;) {
-    cout<<"Vvedi nomer kartochki: ";
-    cin>>N;
-    if ((N>0)&&(N<101))
+    {
+        card[i]=0;
+    }
+    for (;;)
+    {
+        cout<<"Vvedi nomer kartochki: ";
+        cin>>N;
+        if ((N>0)&&(N<101))
         {
             cout<<"Skolko polozhit: ";
             cin>>sum;
             card[N-1]+=sum;
             for (int i=0;i<100;i++)
             cout<<card[i]<<" ";
-            cout<<endl;
+        cout<<endl;
         }
         else
         {
             cout<<"Error: you entered incorrect values!!!\n";
         }
-}
-    return 0;
+    }
+ return 0;
 }
