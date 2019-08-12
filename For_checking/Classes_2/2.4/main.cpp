7b5f23af8f7b0cc706f9e575d5c60a67a22034af
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-12 21:24:54 +0300

    Array data is no longer used

--- For_checking/Classes_2/2.4/main.cpp
+++ For_checking/Classes_2/2.4/main.cpp
@@ -6,30 +6,27 @@ int main()
 {
     int i;
     int stolbik[5];
-    int prototip[5];
     for(i=0;i<5;i++)
     {
         cout<<"Zvezdochek v "<<i+1<<"-om stolbike:";
         cin>>stolbik[i];
-        prototip[i]=stolbik[i];
     }
 
-    int Max=prototip[0];
+    int Max=stolbik[0];
     for (i=1; i<5; i++)
     {
-        if (prototip[i]>Max)
-            Max=prototip[i];
+        if (stolbik[i]>Max)
+            Max=stolbik[i];
     }
     for (int q=0;q<Max;q++)
     {
-    for (i=0;i<5;i++)
-    {
-        if (prototip[i]>0)
-            cout<<"*";
-        else
-            cout<<" ";
-   prototip[i]+=-1;
-    }
+        for (i=0;i<5;i++)
+        {
+            if (stolbik[i]>q)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
     cout<<endl;
     }
     return 0;
