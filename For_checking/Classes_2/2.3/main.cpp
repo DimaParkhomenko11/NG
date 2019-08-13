a66bdc0758b7c9262427495e6ee6cd7952679ce1
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-13 20:14:17 +0300

    Validation for unpaired numbers fixed

--- For_checking/Classes_2/2.3/main.cpp
+++ For_checking/Classes_2/2.3/main.cpp
@@ -10,35 +10,30 @@ int main()
     int End=0;
     do
     {
-        i++;
         cin>>number[i];
-        if (number[i]>0)
+        while ((number[i]%2==0)&&(number[i]!=0))
         {
-            if ((number[i]%2==0))
-                {
-                    cout<<"Error: you entered a pair number!!!";
-                    End=1;
-                    number[i]=0;
-                }
+            cout<<"Error: you entered a pair number!!!\nTry egain: ";
+            cin>>number[i];
         }
-        if (i==19)
-            number[i]=0;
-    }while(number[i]!=0);
-    if (End==0)
-    {
+     i++;
+    }while(number[i-1]!=0 && i<20);
+    //if (End==0)
+   // {
     for (int i=0;i<20;i++)
     {
+        cout<<number[i];
        if (number[i]>0)
            sizze++;
     }
-    int numberMax=number[0];
+   /* int numberMax=number[0];
     for (int i=1; i<20; i++)
     {
         if (number[i]>numberMax)
             numberMax = number[i];
     }
 
-    for (int line=1;line<sizze+1;line++)
+    for (int line=0;line<sizze;line++)
     {
         for (int space=0;(space<(numberMax-number[line])/2);space++)
         {
@@ -49,8 +44,8 @@ int main()
             cout<<"*";
         }
         cout<<endl;
-    }
+    }*/
 
-    }
+   // }
     return 0;
 }
