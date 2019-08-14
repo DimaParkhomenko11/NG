205b5a2b6a77afb819db46af076c0a30523fa45c
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-14 17:20:03 +0300

    Task 3.1 completed

---
+++ For_checking/Classes_3/3.1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    int i=1,q=0;
+    int N=0;
+    char stroka[256]{' '};
+    cin.getline(stroka,256);
+    stroka[0]=' ';
+    while (stroka[i]!=0)
+    {
+        if ((stroka[i]<'a')||(stroka[i]>'z'))
+        if ((stroka[i]<'A')||(stroka[i]>'Z'))
+            stroka[i]=' ';
+        if (stroka[i]!=' ')
+        {
+            if (stroka[i-1]==' ')
+            {
+                N++;
+            }
+        }
+        i++;
+    }
+    cout<<"You have "<<N<<" words";
+    return 0;
+}
