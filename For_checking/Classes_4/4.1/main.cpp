72a4e2f824b5b8c0743b3ca105fe348ddc5405d2
DimaParkhomenko11 <dimonparhom@ukr.net>
2019-08-26 00:27:51 +0300

    Improved input validation system

--- For_checking/Classes_4/4.1/main.cpp
+++ For_checking/Classes_4/4.1/main.cpp
@@ -2,15 +2,31 @@
 
 using namespace std;
 
-void suma(int chislo,int sum)
+void suma(int chislo)
 {
-  if (chislo==0)
-      cout<<"0";
-  else
-  {
-      sum=chislo/10+chislo%10;
-      cout<<"The sum of all digits in the number = "<<sum;
+    if (chislo==0)
+        cout<<"0";
+    else
+    {
+            if (chislo/100>0)
+            {
+                chislo=(chislo/10)+chislo%10;
+                cout<<" YYY"<<chislo<<' ';
+                if (chislo/10>0)
+                {
+
+                    suma(chislo);
+                }
+            }
+            else
+            {
+                chislo=(chislo/10)+chislo%10;
+                cout<<"The sum of all digits in the number = "<<chislo;
+            }
+
+
   }
+
 }
 
 int main()
@@ -19,7 +35,6 @@ int main()
     int chislo;
     cout<<"Enter the number>>";
     cin>>chislo;
-    suma(chislo,sum);
+    suma(chislo);
     return 0;
 }
-
